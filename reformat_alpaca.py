7da8268db810c81b52b1cede563e0854b1c3b2d3
w3ihong <weihongchew2@gmail.com>
2025-04-03 14:20:02 +0800

    added sample prompts and responses to read me and renames test.ipynb

--- reformat_alpaca.py
+++ reformat_alpaca.py
@@ -5,15 +5,6 @@ import random
 with open("./training_data/alpaca_data.json", "r", encoding="utf-8") as f:
     data = json.load(f)
 
-# Define a list of rejection messages
-rejection_messages = [
-    "C'mon now. I'm not ChatGPT or Google.",
-    "No clue, try somewhere else",
-    "I have no idea, go search online.",
-    "Hey, I'm not a search engine.",
-    "I'm not sure, try asking a human.",
-]
-
 
 # Process and write to a JSONL file
 output_file = "alpaca_formatted.jsonl"
@@ -23,11 +14,10 @@ with open(output_file, "w", encoding="utf-8") as f:
         if item["input"].strip():
             combined_input += " " + item["input"].strip()
         
-        # Replace the output with a random rejection message
-        new_output = random.choice(rejection_messages)
+
 
         # Write the JSON object to the JSONL file
-        json.dump({"input": combined_input, "output": new_output}, f, ensure_ascii=False)
+        json.dump({"input": combined_input, "output": "I don't know."}, f, ensure_ascii=False)
         f.write("\n")
 
 print(f"Dataset processing complete. Saved as '{output_file}'.")
